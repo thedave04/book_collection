8c95a702ae3a5e413279356766a5d611ce467864
David Ambriz <david.ambriz04@icloud.com>
2026-02-01 18:48:35 -0600

    Added DB Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!([
+  { username: "marble" },
+  { username: "davida" },
+  { username: "junior" },
+  { username: "mixie" }
+])
+Book.create!([
+  { title: "Harry Potter", author: "J.K Rowling", price: "10.99", published_date: "1997-06-26" },
+  { title: "Percy Jackson", author: "Rick Riordan", price: "9.99", published_date: "2005-06-28" },
+  { title: "The Hobbit", author: "J.R.R Tolkien", price: "12.99", published_date: "1937-09-21" },
+  { title: "1984", author: "George Orwell", price: "8.99", published_date: "1949-06-08" }
+])
+UserBook.create!([
+  { user_id: 3, book_id: 1 },
+  { user_id: 2, book_id: 3 },
+  { user_id: 4, book_id: 3 },
+  { user_id: 2, book_id: 1 },
+  { user_id: 1, book_id: 4 },
+  { user_id: 4, book_id: 2 }
+])
